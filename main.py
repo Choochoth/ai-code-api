b7a2748cf7b6573b2f253d0d3513deaa006d2232
Choochoth <choochoth@gmail.com>
2025-12-16 22:10:46 +0700

    ok

--- main.py
+++ main.py
@@ -136,8 +136,6 @@ def debug_templates_site(site: str = Path(..., description="site name (thai_789b
     return {"site": site, "labels": list(mapping.keys()), "total_images": sum(len(v) for v in mapping.values())}
 
 
-
-
 # ---------------- CORS ----------------
 app.add_middleware(
     CORSMiddleware,
